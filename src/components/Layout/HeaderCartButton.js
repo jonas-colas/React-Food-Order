1387782004f52ebc29399d1c68cfa69b97e29f53
jonascolas <jonas@fusemachines.com>
2022-04-27 21:05:16 -0300

    done

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,7 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
-import { useState } from 'react';
 
 const HeaderCartButton = props => {
   const ctx = useContext(CartContext);
